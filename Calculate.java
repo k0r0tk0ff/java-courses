c9d644fef4582840f50af7d2cc7eef087cdef5f7
k0r0tk0ff <korotkov34reg@gmail.com>
2016-07-21 14:49:43 +0300

    create Calculate

---
+++ Calculate.java
@@ -0,0 +1,8 @@
+public class Calculate {
+// Имя класса и файла должны быть одинаковыми ! Регистрозависимы!!!
+// D:\JAVA\projects\Calculate.java
+
+		public static void main (String[] arg){
+			System.out.println("Calculate ...");
+		}
+}
