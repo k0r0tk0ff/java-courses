206a3ae705ee60c5f184ba94aacad27224d71929
k0r0tk0ff <korotkov34reg@gmail.com>
2016-08-27 19:55:51 +0300

    first commit

--- Calculate.java
+++ Calculate.java
@@ -1,8 +1,5 @@
 public class Calculate {
 
-// D:\JAVA\projects\Calculate.java
-
-		public static void main (String[] arg){
-			System.out.println("Calculate ...");
+		public static void main (String[] arg){System.out.println("Calculate ...");
 		}
 }
