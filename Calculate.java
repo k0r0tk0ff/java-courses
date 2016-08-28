e1c25c8baaba5efc5e60a046c65696a655737902
k0r0tk0ff <korotkov34reg@gmail.com>
2016-08-28 20:00:05 +0300

    attempt to add class Shell with function toShell

--- Calculate.java
+++ Calculate.java
@@ -1,53 +1,84 @@
+import java.util.ArrayList;
+
 public class Calculate {
-		public static void main (String[] arg) throws Exception{
+		public static void main (String[] arg) throws Exception {
 			System.out.println("Calculate ...");
-
-			//int first = Integer.valueOf(arg[0]);
-			//int second = Integer.valueOf(arg[1]);
-			//int sum = first + second;
-
-			//System.out.println("\n First argument  is "+arg[0]);
-			//System.out.println("Second argument  is "+arg[1]);
-
-			//System.out.println("\n Sum is "+sum);
-
 			String aaa1 = new String("1");
 			String aaa2 = new String("99999999999999999999999999999999999999999999999999999");
 
 			//System.out.println(" The first argument is "+aaa1);
 			//System.out.println(" The second argument is "+aaa2);
 
-			System.out.println(" The first argument: "); 	DeterminantedArg a1 = new DeterminantedArg(aaa1);
-			System.out.println(" The second argument: ");    DeterminantedArg a2 = new DeterminantedArg(aaa2);
-
-			System.out.println("\n The first argument is "+a1);
-			System.out.println(" The second argument is "+a2);
-
-			//try { int AA = Double.parseDouble(arg2); System.out.println(" The second argument is int ");}
-			//catch(NumberFormatException error){System.out.println(" The first argument is not int ");}
+			//System.out.println(" The first argument: "); 	DeterminantedArg a1 = new DeterminantedArg(aaa1);
+			//System.out.println(" The second argument: ");    DeterminantedArg a2 = new DeterminantedArg(aaa2);
 
 
-			// etalon
-			//try { int WW = Integer.parseInt(arg1); System.out.println(" The first argument is int ");}
-			//catch(Exception e){System.out.println(" The first argument is not int ");}
+			//ArgArray.add(4);
+			//ArgArray.add(4);
+
+			//DeterminantedArg(aaa1);
+			//DeterminantedArg(aaa2);
+
+			//double sum = ArgArray.get(0) + ArgArray.get(1);
+			//System.out.println(" Sum is " + sum);
+
+			Shell a = new Shell();
+			a.ArgToShell(aaa1, aaa2);
+
 		}
-static class DeterminantedArg{
-	public DeterminantedArg(String input) {
-		try { final short var = Short.parseShort(input); System.out.println(" Type of the argument is short");}
-		catch(NumberFormatException error1){
-			try { final int var = Integer.parseInt(input); System.out.println(" Type of the argument is int ");}
-			catch(NumberFormatException error2){
-				try { final float var = Float.parseFloat(input); System.out.println(" Type of the argument is float");}
-				catch(NumberFormatException error3) {
-					try { final double var = Double.parseDouble(input); System.out.println(" Type of the argument is double");}
-					catch(NumberFormatException error4) {
-						try { final long var = Long.parseLong(input); System.out.println(" Type of the argument is long.");}
-						catch(NumberFormatException error5) {}
+}
+
+//public class NumberFormatError extends Exception {
+//	public String getMessage(){return "Type is illegel...";}
+//}
+
+class Shell {
+	static String ArgToShell(String[] input) {
+		ArrayList ArgArray = new ArrayList(2);
+
+		for(int i=0; i<2; i++) {
+		try {
+			final short var = Short.parseShort(input[i]);
+			System.out.println(" Type of the argument is short");
+			ArgArray.add(var);
+
+		} catch (NumberFormatException error1) {
+			try {
+				final int var = Integer.parseInt(input[i]);
+				System.out.println(" Type of the argument is int ");
+				ArgArray.add(var);
+			} catch (NumberFormatException error2) {
+				try {
+					final float var = Float.parseFloat(input[i]);
+					System.out.println(" Type of the argument is float");
+					ArgArray.add(var);
+				} catch (NumberFormatException error3) {
+					try {
+						final double var = Double.parseDouble(input[i]);
+						System.out.println(" Type of the argument is double");
+						ArgArray.add(var);
+					} catch (NumberFormatException error4) {
+						try {
+							final long var = Long.parseLong(input[i]);
+							System.out.println(" Type of the argument is long.");
+							ArgArray.add(var);
+						} catch (NumberFormatException error5) {
+							System.out.println(" Type of the argument is not long.");
+						}
 					}
 				}
 			}
 		}
+		finally { System.out.println("Parse the "+i+" arg is complete");
+
+		}
+
 	}
-}
+		double a1 = double(ArgArray.get(0));
+		double a2 = double(ArgArray.get(1));
+
+		double sum = a1 + a2; //ArgArray.get(0) + ArgArray.get(1);
+		return "The sum is"+sum;
+	}
 
 }
